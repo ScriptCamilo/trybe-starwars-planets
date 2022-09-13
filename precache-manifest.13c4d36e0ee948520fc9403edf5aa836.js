a6e6741850f1dfd4187a3f7755dfb9ea0c60b3f4
ScriptCamilo <script.camilo@gmail.com>
2022-09-13 19:53:23 -0300

    feat: deploy React app to GitHub Pages

---
+++ precache-manifest.13c4d36e0ee948520fc9403edf5aa836.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a77b773ec3d881565dec7ce0e4e5b1f",
+    "url": "/trybe-starwars-planets/index.html"
+  },
+  {
+    "revision": "593b60e0df1fe0205e3a",
+    "url": "/trybe-starwars-planets/static/css/main.c9a44ff0.chunk.css"
+  },
+  {
+    "revision": "6f75abd2137825409f5e",
+    "url": "/trybe-starwars-planets/static/js/2.ead56935.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/trybe-starwars-planets/static/js/2.ead56935.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "593b60e0df1fe0205e3a",
+    "url": "/trybe-starwars-planets/static/js/main.a797896b.chunk.js"
+  },
+  {
+    "revision": "9926e31e2d9d0f9f56f6",
+    "url": "/trybe-starwars-planets/static/js/runtime-main.f28a19c2.js"
+  }
+]);
